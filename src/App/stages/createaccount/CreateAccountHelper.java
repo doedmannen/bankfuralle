27bd1ea6de0e23f017a25ede276d6df01daf39b2
Kristoffer <doedmannen@aetmej.se>
2019-03-23 22:00:42 +0100

    Function for parsing accounttype

--- src/App/stages/createaccount/CreateAccountHelper.java
+++ src/App/stages/createaccount/CreateAccountHelper.java
@@ -1,7 +1,23 @@
 package App.stages.createaccount;
 
 public class CreateAccountHelper {
-    void createNewaccount(Object ... data){
+    static void createNewaccount(Object ... data){
 
+    }
+
+    static String parseAccountType(String type){
+        String returnValue;
+        switch (type){
+            case "Kortkonto":
+                returnValue = "CARD";
+                break;
+            case "Sparkonto":
+                returnValue = "SAVING";
+                break;
+            default:
+                returnValue = "Lönekonto";
+                break;
+        }
+        return returnValue;
     }
 }
